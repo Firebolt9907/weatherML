10dbfb55fbbf7a4b1e50d1debc745d563895840b
Firebolt9907 <rishushar99@gmail.com>
2024-02-26 07:24:55 -0600

    minor changes - not sure if it works

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 import torch
 import pandas as pd
+import re
 
 df = pd.read_csv('weather.csv')
-
-null_columns = df.columns[df.isnull().any()]
 
 # newdf = df.drop(columns=null_columns)
 
 # print(df.columns)
 
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+filterDf = df[['DATE', 'HourlyDryBulbTemperature']].copy().dropna()
+
 tensors = []
-inputs = pd.to_datetime(df['DATE'], format='%Y-%m-%dT%H:%M:%S')
-tensors.append(torch.tensor(inputs.dt.year))
-tensors.append(torch.tensor(inputs.dt.month))
-tensors.append(torch.tensor(inputs.dt.day))
-tensors.append(torch.tensor(inputs.dt.hour))
-tensors.append(torch.tensor(inputs.dt.hour))
+inputs = pd.to_datetime(filterDf['DATE'], format='%Y-%m-%dT%H:%M:%S')
+tensors.append(torch.tensor(inputs.dt.year.values).to(device))
+tensors.append(torch.tensor(inputs.dt.month.values).to(device))
+tensors.append(torch.tensor(inputs.dt.day.values).to(device))
+tensors.append(torch.tensor(inputs.dt.hour.values).to(device))
+tensors.append(torch.tensor(inputs.dt.hour.values).to(device))
 
 layers = [5, 200, 800, 1500, 800, 400, 200, 1]
 
-actualOutputs = []
+actualOutputs = [int(temp.replace("s", '')) if isinstance(temp, str) else temp for temp in filterDf['HourlyDryBulbTemperature']]
 predictedOutputs = []
 
-tensor_thing = torch.FloatTensor(tensors)
+tensor_thing = torch.FloatTensor(tensors).to(device)
 
 
 class H(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        for f in range(len(layers)-1):
+        for f in range(len(layers) - 1):
             if f != len(layers) - 1:
-                self.input_layer_exe[f] = torch.nn.Linear(layers[f], layers[f + 1])  # input: x value
+                self.input_layer_exe[f] = torch.nn.Linear(
+                    layers[f], layers[f + 1])  # input: x value
         self.Activate = torch.nn.ReLU()
         # self.output_layer_exe = torch.nn.Linear(10, 1)  # output: y value
 
