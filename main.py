d7e5551780f233fdb9434fbb7fd706c62e0fde23
Firebolt_9907 <rishushar99@gmail.com>
2024-03-08 14:12:53 -0600

    forgot to commit main.py

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
 import torch
 import pandas as pd
+from sklearn.utils import shuffle
 import re
 
 df = pd.read_csv('weather.csv')
 
 LEARN_RATE = 0.00025
 
+print(1)
+
 device = (
     "cuda"
     if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
+    # else "mps"
+    # if torch.backends.mps.is_available()
     else "cpu"
 )
 
 filterDf = df[['DATE', 'HourlyDryBulbTemperature']].dropna()
 
+print(2)
+
+filterDf = filterDf.iloc[-1:1000]
+
+print(3)
+
 inputs = []
 dates = pd.to_datetime(filterDf['DATE'], format='%Y-%m-%dT%H:%M:%S')
 for date in dates:
     inputs.append([date.year, date.month,
-                  date.day, date.hour, date.minute])
+                   date.day, date.hour, date.minute])
+
+print(4)
 
 # layers = [4, 200, 800, 1500, 800, 400, 200, 1]
-print(filterDf.head(8117))
-exit()
 
 actualOutputs = [int(temp.replace("s", '')) if isinstance(
     temp, str) else temp for temp in filterDf['HourlyDryBulbTemperature']]
 predictedOutputs = []
 
+print(5)
+
 tensor_thing = torch.FloatTensor(inputs).to(device)
 
+print(6)
 
 class Model(torch.nn.Module):
     def __init__(self):
@@ -59,23 +71,27 @@ class Model(torch.nn.Module):
         return input
 
 
-
 model = Model().to(device)
-loss_function = torch.nn.HuberLoss(reduction = 'mean')
+loss_function = torch.nn.MSELoss(reduction='mean')
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARN_RATE)
 
+print(7)
+
+output = torch.FloatTensor(actualOutputs).to(device)
+
+print(8)
+
 for i in range(len(inputs)):
+    print(9)
     inp = torch.FloatTensor(inputs[i]).to(device)
-    outp = torch.FloatTensor(actualOutputs[i]).to(device)
 
     prediction = model(inp)
-    loss = loss_function(prediction, outp)
+    loss = loss_function(prediction, output[i])
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
-    print(f"current loss at day {str(inputs[i][1])}/{str(inputs[i][2])}/{str(inputs[i][0] % 100)} at {str(inputs[i][3])}:{str(inputs[i][4])}, iteration {str(i)}: {str(loss.item())}")
-    if loss.item() < 400:
-        LEARN_RATE = 0.000025
-    elif loss.item() < 40:
-        LEARN_RATE = 0.0000025
+    print(10)
+    print(
+        f"current loss at day {str(inputs[i][1])}/{str(inputs[i][2])}/{str(inputs[i][0] % 100)} at {str(inputs[i][3])}:{str(inputs[i][4])}, iteration {str(i)} loss: {str(loss.item())}, predicted: {str(prediction.item())}, actual: {str((output[i].item()))}")
 
+torch.save(model.state_dict(), 'model.pt')
