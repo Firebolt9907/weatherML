0675aed98faa6fce77a7a70654ec389b2d0d2ee7
Firebolt_9907 <rishushar99@gmail.com>
2024-03-08 14:09:15 -0600

    I made it work then I broke it

---
+++ test.py
@@ -0,0 +1,45 @@
+import torch
+
+# from weatherML.main import Model
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = torch.nn.Linear(5, 200)
+        self.layer2 = torch.nn.Linear(200, 800)
+        self.layer3 = torch.nn.Linear(800, 1500)
+        self.layer4 = torch.nn.Linear(1500, 800)
+        self.layer5 = torch.nn.Linear(800, 200)
+        self.layer6 = torch.nn.Linear(200, 1)
+        self.Activate = torch.nn.ReLU()
+
+    def forward(self, input):
+        input = self.layer1(input)
+        input = self.Activate(input)
+        input = self.layer2(input)
+        input = self.Activate(input)
+        input = self.layer3(input)
+        input = self.Activate(input)
+        input = self.layer4(input)
+        input = self.Activate(input)
+        input = self.layer5(input)
+        input = self.Activate(input)
+        input = self.layer6(input)
+        return input
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+model = Model()
+model.load_state_dict(torch.load('model.pt', map_location=torch.device(device)))
+model.to(device)
+
+# year, month, day, hour, minute (CAN ONLY BE 15, 35, OR 55),
+inp = torch.FloatTensor([2015, 1, 1, 1, 15]).to(device)
+
+print(model(inp))
